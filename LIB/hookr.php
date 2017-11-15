0605b9321b27707bfaedb7bed1f0f3642a5074db
Zach Thompson <thompzac@oregonstate.edu>
2017-11-14 19:08:45 -0800

    Added functions within HookR class The HookR system will use classes for each hook to dynamically call hooking functions.

--- LIB/hookr.php
+++ LIB/hookr.php
@@ -20,10 +20,56 @@
 class HookR{
 	private $hookRGX = "/\{\%[a-zA-Z_\-\>]+\}/g"; //The regular expression to match all hooks
 
-	public function getHooks($input){
+
+	//Get an array of the hooks contained within $input
+	private function getHooks($input){
 		$hooks;
-		$hookCount = preg_match_all($this->hookRGX, $input, $hooks);	
+		$hookCount = preg_match_all($this->hookRGX, $input, $hooks);
+		return $hooks;	
 	}
+
+
+	//Check if the basehook is a valid hook
+	private function validateBaseHook($basehook){
+		if(class_exists($basehook))return true;
+		return false;
+	}
+
+
+	//Check if a basehook has an extension and is a valid complex hook
+	private function baseHasExtension($basehook, $extension){
+		if(validateBaseHook($basehook)){
+			if(method_exists($basehook, $extension))return true;
+		}
+		return false;
+	}
+
+
+	//Translate the hook into actual data
+	//INPUT $hook: The hook string to translate
+	//INPUT $context: A context object that contains information about the context of the hook
+	private function translateHook($hook, $context){
+		$outputData;
+		//Check if the hook is a basic hook or a complex hook
+		if(strpos($hook, "->") !== false){
+			$base = explode($hook, "->")[0];
+			$extension = explode($hook, "->")[1];
+			if(baseHasExtension($base, $extension)){
+				$hookClass = new $base();
+				$outputData = $hookClass->$extension($context);
+			}
+		}
+		else {
+			if(validateBaseHook($hook)){
+				$hookClass = new $hook($context);
+				$outputData = $hookClass();
+			}
+		}
+
+		return $outputData;
+	}
+
+	
 }
 
 ?>
