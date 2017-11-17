21af786fe632a7224dc765a8631af7de1292b80a
Zach <thompzac@oregonstate.edu>
2017-11-16 22:26:09 -0800

    Started working on installer templates Beginning to block out the full installer

--- installer/db.php
+++ installer/db.php
@@ -7,6 +7,10 @@
 
 $userq = "CREATE TABLE users (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, username varchar(255) UNIQUE NOT NULL, email varchar(255) NOT NULL, fullname varchar(255) NOT NULL, passhash varchar(255) NOT NULL, primarygid int(255) NOT NULL, gids varchar(255) NOT NULL, adminnotes varchar(255), registered DATETIME NOT NULL)";
 
+$sessq = "CREATE TABLE sessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
+
+$admsessq = "CREATE TABLE adminsessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
+
 $templateq = "CREATE TABLE templates (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
 
 $styleq = "CREATE TABLE styles (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
@@ -19,7 +23,9 @@ $bpostq = "CREATE TABLE fposts (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMA
 
 $bcommentq = "CREATE TABLE fcomments (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL, replyto int(255) NOT NULL)";
 
-$groupsq = "CREATE TABLE groups (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, level int(255) NOT NULL, imagepath varchar(255))";
+$groupsq = "CREATE TABLE groups (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, parent int(255), level int(255) NOT NULL, imagepath varchar(255))";
+
+$pagesq = "CREATE TABLE pages (id int(255) AUTO_INCREMENT UNIQUE NOTNULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, title varchar(255), mastertemplate int(255) NOT NULL, gid int(255) NOT NULL)";
 
 
 ?>
--- installer/index.php
+++ installer/index.php
@@ -2,5 +2,68 @@
 //installer/index.php
 
 //This is the starting page for the installer program.
+$maintemplate = file_get_contents("./templates/main.html");
+
+$progress = "Start";
+$body = "Need Template!";
+
+if(!file_exists("./beacon.md")){
+	//already installed????
+	$progress = "Already Installed!";
+	$body = file_get_contents("./templates/alreadyinstalled.html");
+	$maintemplate = preg_replace("/\{progress\}/", $progress, $maintemplate);
+	$maintemplate = preg_replace("/\{content\}/", $body, $maintemplate);
+	echo $maintemplate;
+}
+
+if(isset($_GET['progress']) || isset($_POST['progress'])){
+	//handle progression and continue
+	
+	$progressvar = "start";
+	if(isset($_GET['progress'])) $progressvar = $_GET['progress'];
+	else $progressvar = $_POST['progress'];	
+
+	switch($progressvar){
+		case 'sqlsetup': doSQLSetup();
+			break;
+		default: invalidPage();
+			break;
+	}
+
+	echo $maintemplate;
+}
+else {
+	//No progression, start from beginning
+
+	$progress = "Welcome";
+	$body = file_get_contents("./templates/welcome.html");
+
+	$maintemplate = preg_replace("/\{progress\}/", $progress, $maintemplate);
+	$maintemplate = preg_replace("/\{content\}/", $body, $maintemplate);
+	echo $maintemplate;
+}
+
+function insertData($template, $progress, $body){
+	$temp = $template;
+	$temp = preg_replace("/\{progress\}/", $progress, $temp);
+	$temp = preg_replace("/\{content\}/", $body, $temp);
+	return $temp;
+}
+
+function invalidPage(){
+	global $maintemplate;
+	$progress = "Invalid Page";
+	$body = file_get_contents("./templates/wrongturn.html");
+	$maintemplate = insertData($maintemplate, $progress, $body);
+	echo $maintemplate;
+}
+
+function doSQLSetup(){
+	global $maintemplate;
+	$progress = "Welcome/SQL Setup";
+	$body = file_get_contents("./templates/sqlsetup.html");
+	$maintemplate = insertData($maintemplate, $progress, $body);
+	echo $maintemplate;
+}
 
 ?>
