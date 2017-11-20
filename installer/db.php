38d72869ee10923dfde217ae8d5c05b04411af6a
Zach Thompson <thompzac@oregonstate.edu>
2017-11-20 03:00:06 -0800

    First installer page is now working. Asynchronously checks the given credentials. Only allows user to continue if the database connection succeeds.

--- installer/db.php
+++ installer/db.php
@@ -1,31 +1,157 @@
 <?php
+require_once 'dbcreds.php';
+session_start();
 //installer/db.php
 
 //This file contains the code to actually create and populate the database
 
 //Variables
 
-$userq = "CREATE TABLE users (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, username varchar(255) UNIQUE NOT NULL, email varchar(255) NOT NULL, fullname varchar(255) NOT NULL, passhash varchar(255) NOT NULL, primarygid int(255) NOT NULL, gids varchar(255) NOT NULL, adminnotes varchar(255), registered DATETIME NOT NULL)";
+$tableprefix = "";
 
-$sessq = "CREATE TABLE sessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
+$userq = "CREATE TABLE ".$tableprefix."users (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, username varchar(255) UNIQUE NOT NULL, email varchar(255) NOT NULL, fullname varchar(255) NOT NULL, passhash varchar(255) NOT NULL, primarygid int(255) NOT NULL, gids varchar(255) NOT NULL, adminnotes varchar(255), registered DATETIME NOT NULL)";
 
-$admsessq = "CREATE TABLE adminsessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
+$sessq = "CREATE TABLE ".$tableprefix."sessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
 
-$templateq = "CREATE TABLE templates (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
+$admsessq = "CREATE TABLE ".$tableprefix."adminsessions (id varchar(255) UNIQUE NOT NULL PRIMARY KEY, userid int(255) UNIQUE NOT NULL, start DATETIME NOT NULL, expire DATETIME NOT NULL)";
 
-$styleq = "CREATE TABLE styles (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
+$templateq = "CREATE TABLE ".$tableprefix."templates (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
 
-$fpostq = "CREATE TABLE fposts (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, sectiontree varchar(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL)";
+$styleq = "CREATE TABLE ".$tableprefix."styles (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, hook varchar(255) UNIQUE NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, content varchar(10240) NOT NULL)";
 
-$fcommentq = "CREATE TABLE fcomments (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL, replyto int(255) NOT NULL)";
+$fpostq = "CREATE TABLE ".$tableprefix."fposts (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, sectiontree varchar(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL)";
 
-$bpostq = "CREATE TABLE fposts (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, sectiontree varchar(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL)";
+$fcommentq = "CREATE TABLE ".$tableprefix."fcomments (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL, replyto int(255) NOT NULL)";
 
-$bcommentq = "CREATE TABLE fcomments (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL, replyto int(255) NOT NULL)";
+$bpostq = "CREATE TABLE ".$tableprefix."fposts (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, sectiontree varchar(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL)";
 
-$groupsq = "CREATE TABLE groups (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, parent int(255), level int(255) NOT NULL, imagepath varchar(255))";
+$bcommentq = "CREATE TABLE ".$tableprefix."fcomments (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, author int(255) NOT NULL, created DATETIME NOT NULL, lastedit DATETIME NOT NULL, body varchar(10240) NOT NULL, replyto int(255) NOT NULL)";
 
-$pagesq = "CREATE TABLE pages (id int(255) AUTO_INCREMENT UNIQUE NOTNULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, title varchar(255), mastertemplate int(255) NOT NULL, gid int(255) NOT NULL)";
+$groupsq = "CREATE TABLE ".$tableprefix."groups (id int(255) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, parent int(255), level int(255) NOT NULL, imagepath varchar(255))";
 
+$pagesq = "CREATE TABLE ".$tableprefix."pages (id int(255) AUTO_INCREMENT UNIQUE NOTNULL PRIMARY KEY, name varchar(255) UNIQUE NOT NULL, title varchar(255), mastertemplate int(255) NOT NULL, gid int(255) NOT NULL)";
+
+if(isset($_POST['verify'])){
+	if(!isset($_POST['host']) || !isset($_POST['user']) || !isset($_POST['pass']) || !isset($_POST['database'])){
+		echo "nodata";
+	}
+	else {
+		$host = $_POST['host'];
+		$user = $_POST['user'];
+		$pass = $_POST['pass'];
+		$database = $_POST['database'];
+		$testConn = mysqli_connect($host, $user, $pass, $database);
+		if(!$testConn) echo "error";
+		else {
+			echo "connected";
+			mysqli_close($testConn);
+	}
+}
+return;
+//die();//Die to be sure that no more code gets executed
+}
+
+if(isset($_POST['startsession'])){
+	//setup the session for further work
+	if(!isset($_POST['host']) || !isset($_POST['user']) || !isset($_POST['pass']) || !isset($_POST['database'])){
+		echo "error";
+	}
+	else{
+		$_SESSION['DBLOGINDONE'] = "TRUE";
+		$_SESSION['DBHOST'] = $_POST['host'];
+		$_SESSION['DBUSER'] = $_POST['user'];
+		$_SESSION['DBPASS'] = $_POST['pass'];
+		$_SESSION['DBDB'] = $_POST['database'];
+		echo "done";
+	}
+	return;
+}
+
+if(!isset($_SESSION['DBLOGINDONE']))die("The database credentials have not yet been setup!");
+if(!isset($_SESSION['DBHOST'])) die("Database settings not set!");
+if(!isset($_SESSION['DBUSER'])) die("Database settings not set!");
+if(!isset($_SESSION['DBPASS'])) die("Database settings not set!");
+if(!isset($_SESSION['DBDB'])) die ("Database settings not set!");
+
+
+if(isset($_GET['debug'])){
+	doDebug();
+}
+
+$hostname = $_SESSION['DBHOST'];
+$username = $_SESSION['DBUSER'];
+$pass = $_SESSION['DBPASS'];
+$database = $_SESSION['DBDB'];
+
+$conn = mysqli_connect($hostname, $username, $pass, $database);
+if(!$conn) die ("Failed to connect to database: " . mysqli_connect_error());
+
+if(isset($_POST['DO'])){
+	$action = $_POST['DO'];
+	switch($action){
+		case "debug_test": doDebug();
+			break;
+		case "user_setup": runUserSetup();
+			break;
+		case "forum_setup": runForumSetup();
+			break;
+		case "forum_comment_setup": runFCommentSetup();
+			break;
+		case "blog_setup": runBlogSetup();
+			break;
+		case "blog_comment_setup": runBCommentSetup();
+			break;
+		case "sess_setup": runSessSetup();
+			break;
+		case "adm_sess_setup": runAdmSessSetup();
+			break;
+		case "group_setup": runGroupSetup();
+			break;
+		case "template_setup": runTemplateSetup();
+			break;
+		case "style_setup": runStyleSetup();
+			break;
+		case "page_setup": runPageSetup();
+			break;
+		default: echo "Nothing to do";
+			break;
+	}
+}
+
+function doDebug(){
+	echo $GLOBALS["userq"] . "<br>";
+	echo $GLOBALS["sessq"] . "<br>";
+	echo $GLOBALS["admsessq"] . "<br>";
+	echo $GLOBALS["templateq"] . "<br>";
+	echo $GLOBALS["styleq"] . "<br>";
+	echo $GLOBALS["fpostq"] . "<br>";
+	echo $GLOBALS["fcommentq"] . "<br>";
+	echo $GLOBALS["bpostq"] . "<br>";
+	echo $GLOBALS["bcommentq"] . "<br>";
+	echo $GLOBALS["groupsq"] . "<br>";
+	echo $GLOBALS["pagesq"] . "<br>";
+}
+
+function runUserSetup(){}
+
+function runForumSetup(){}
+
+function runFCommentSetup(){}
+
+function runBlogSetup(){}
+
+function runBCommentSetup(){}
+
+function runSessSetup(){}
+
+function runAdmSessSetup(){}
+
+function runGroupSetup(){}
+
+function runTemplateSetup(){}
+
+function runStyleSetup(){}
+
+function runPageSetup(){}
 
 ?>
---
+++ installer/js/master.js
@@ -0,0 +1,65 @@
+function testConnection(){
+	var host,user,pass,database;
+
+	host = document.getElementById("dbhost").value;
+	user = document.getElementById("dbuser").value;
+	pass = document.getElementById("dbpass").value;
+	database = document.getElementById("dbdatabase").value;
+	var output = document.getElementById("testResult");
+	var button = document.getElementById("sqlsetup-forward-button");
+	var link = document.getElementById("sqlsetup-forward-link");
+	if(host == "" || user == "" || pass == "" || database == ""){
+		button.className = "disabled-button";
+		link.removeAttribute("href");
+		output.innerHTML = "";
+		return;
+	}
+		
+	button.className = "disabled-button";
+	link.removeAttribute("href");
+	output.innerHTML = "";
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			if(this.response == "connected"){
+				output.innerHTML = "Connection Established";
+				output.className = "dbconnect";
+				installSession(host, user, pass, database);
+			}
+			else {
+				button.className = "disabled-button";
+				link.removeAttribute("href");
+				output.innerHTML = "Database connection error. Please check your credentials";
+				output.className = "dbfail";
+			}
+		}
+	}
+	xhttp.open("POST", "db.php", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send("verify=true&host=" + host + "&user=" + user + "&pass=" + pass + "&database=" + database);
+}
+
+function installSession(host, user, pass, database){
+	var button = document.getElementById("sqlsetup-forward-button");
+	var link = document.getElementById("sqlsetup-forward-link");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			if(this.response == "done") {
+
+				button.className = "forward-button";
+				link.href="./?progress=sqlinstall";
+				//console.log("session has been installed");
+			}
+			else {
+				link.href = null;
+				button.className = "disabled-button";
+				console.log("an error has occured");
+				console.log(this.response);
+			}
+		}
+	}
+	xhttp.open("POST", "db.php", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send("startsession=true&host=" + host + "&user=" + user + "&pass=" + pass + "&database=" + database);
+}
