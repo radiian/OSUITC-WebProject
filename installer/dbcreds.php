8021ecf22807dc97bac55a53ebc566dca6b9bf51
Zach <thompzac@oregonstate.edu>
2017-11-19 22:54:28 -0800

    Error logs should be assumed-unchanged. Oops

---
+++ installer/dbcreds.php
@@ -0,0 +1,9 @@
+<?php
+	
+	//Default database credentials
+	//This file will get overwritten during install
+	$db_host = "127.0.0.1";
+	$db_user = "dbuser";
+	$db_pass = "verybadpass";
+	$db_name = "mydatabase";
+?>
